06155f7761e436dafb7e87df0cd23379015d8fd0
SiyuanGuo <guosiyuan@berkeley.edu>
2017-04-10 23:10:22 -0700

    commit 3

--- SnapchatClonePt3/LoginViewController.swift
+++ SnapchatClonePt3/LoginViewController.swift
@@ -41,8 +41,29 @@ class LoginViewController: UIViewController, UITextFieldDelegate {
     @IBAction func didAttemptLogin(_ sender: UIButton) {
         guard let emailText = emailField.text else { return }
         guard let passwordText = passwordField.text else { return }
-        
-        // YOUR CODE HERE
+        FIRAuth.auth()?.signIn(withEmail: emailText, password: passwordText) { (user, error) in
+            
+            if error == nil {
+                
+                //Print into the console if successfully logged in
+                print("You have successfully logged in")
+                
+                //Go to the HomeViewController if the login is sucessful
+                let vc = self.storyboard?.instantiateViewController(withIdentifier: "Home")
+                self.present(vc!, animated: true, completion: nil)
+                
+            } else {
+                
+                //Tells the user that there is an error and then gets firebase to tell them the error
+                let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+        // YOUR CODE HERE Done
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
